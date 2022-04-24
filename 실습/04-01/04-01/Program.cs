6ef7ebfec598855c5f838b090a1643323f8e8033
dlfpire <azx7947@naver.com>
2022-04-25 03:23:17 +0900

    0425

---
+++ 실습/04-01/04-01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01
+{
+    public delegate void MyEventHandler();
+    class Button
+    {
+        public event MyEventHandler Push;
+        public void OnPush()
+        {
+            if(Push != null)
+                Push();
+        }
+    }
+    class EventHandlerClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("In the EventHandlerClass.MyMethod ...");
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Button button = new Button();
+            EventHandlerClass obj = new EventHandlerClass();
+            button.Push += new MyEventHandler(obj.MyMethod);
+            button.OnPush();
+        }
+    }
+}
---
+++ 실습/04-01/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Class
+{
+	public class Class1
+	{
+		public Class1()
+		{
+			A a = new A();
+			a.k = 1;
+			
+		}
+	}
+}
